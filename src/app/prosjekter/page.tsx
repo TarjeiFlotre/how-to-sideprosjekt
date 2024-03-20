3c99710e301d03ad7764ae30cebf66dfc7640ad2
Tarjei Flotre <tarjei.flotre@gmail.com>
2024-03-20 18:00:41 +0100

    added prosjekter

---
+++ src/app/prosjekter/page.tsx
@@ -0,0 +1,10 @@
+// src/app/prosjekter/page.tsx
+import { ProjectList } from "./../components/project-list";
+
+export default function ProjectPage() {
+    return (
+      <div>
+        <ProjectList />
+      </div>
+    );
+  }
