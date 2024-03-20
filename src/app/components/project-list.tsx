013cad50bf1e41a6d39659bd8097148aa33bf28c
Tarjei Flotre <tarjei.flotre@gmail.com>
2024-03-20 17:32:57 +0100

    prosjektliste

---
+++ src/app/components/project-list.tsx
@@ -0,0 +1,21 @@
+export function ProjectList() {
+    return (    
+        <div className="text-center"> 
+            <h1 className="text-2xl">Prosjektliste</h1>
+            <ol>
+                <li>
+                    <h2>Prosjekt 1</h2>
+                    <p>Test</p>
+                </li>
+                <li>
+                    <h2>Prosjekt 2</h2>
+                    <p>Test test</p>
+                </li>
+                <li>
+                    <h2>Prosjekt 3</h2>
+                    <p>Test test test</p>
+                </li>
+            </ol>
+        </div>
+    )
+    }
