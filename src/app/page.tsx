3a68535224aeabe6987514f825077b30b761bc14
Tarjei Flotre <tarjei.flotre@gmail.com>
2024-03-20 17:19:17 +0100

    oppgave 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 export default function Home() {
   return (
-    <main>
-      <h1>Heisann</h1>
-        <p>Dette er en testnettside lagd av Tarjei</p>
-        <div>
-          <p>Tester div bokser</p>
+        <div className="text-center">
+          <h1 className="text-2xl">Heisann, jeg heter Tarjei</h1>
+            <p>Dette er en testnettside lagd av Tarjei</p>
+            <p>Tester div bokser</p>
+            <h1>Ting jeg skal gjøre i dag</h1>
+            <ul>
+              <li>Lære dette</li>
+              <li>Gjøre sideprosjekt</li>
+            </ul>
         </div>
-    </main>
   );
 }
