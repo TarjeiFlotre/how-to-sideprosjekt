54e5b1ee66fe2729f160b9a2d50753682d8b9734
Tarjei Flotre <tarjei.flotre@gmail.com>
2024-03-20 17:14:49 +0100

    test 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,10 @@ export default function Home() {
   return (
     <main>
       <h1>Heisann</h1>
+        <p>Dette er en testnettside lagd av Tarjei</p>
+        <div>
+          <p>Tester div bokser</p>
+        </div>
     </main>
   );
 }
